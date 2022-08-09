d9bc0b5215682ea584aabd655524314f3be3b1a2
Daniel <daniel-mcintyre@hotmail.com>
2022-08-09 13:14:36 -0400

    navbar and route setup

--- e-comm-frontend/src/App.js
+++ e-comm-frontend/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './Components/navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route index element={HomePage} />
+          <Route path="*" element={HomePage} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
