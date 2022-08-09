b1acb8040186221f902dcfd6289b646de9f5bb3f
Daniel <daniel-mcintyre@hotmail.com>
2022-08-09 14:49:03 -0400

    updated homepage

--- e-comm-frontend/src/Pages/home.js
+++ e-comm-frontend/src/Pages/home.js
@@ -1,9 +1,31 @@
+import { Link } from "react-router-dom";
+import Row from 'react-bootstrap/Row';
 
 function HomePage() {
     return (
         <div>
-            <p>Home Page</p>
+            <div>
+        <div className="products-container">
+            <h2>Last Products</h2>
+            {/* last products here from backend */}
         </div>
+            <div>
+            <Link to="/category/all">See More</Link>
+            </div>
+            </div>
+            {/* sale banner from canva */}
+        <div className="sale-banner-container">
+            <img src="" alt="" />
+            </div>
+        <div>
+                <div className="recent-products-container">
+                    <h2>Catergories</h2>
+                    <Row>
+
+                    </Row>
+            </div>   
+        </div>
+    </div>
     )
 }
 
