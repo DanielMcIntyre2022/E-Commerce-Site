3dd85fa8fee6b8e06c91b97a56532bf3738d55b9
Daniel <daniel-mcintyre@hotmail.com>
2022-08-09 14:28:17 -0400

    created signup page form with state inputsand functions

--- e-comm-frontend/src/App.js
+++ e-comm-frontend/src/App.js
@@ -4,6 +4,7 @@ import NavBar from './Components/navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './Pages/home';
 import LoginPage from './Pages/login';
+import SignUpPage from './Pages/signup';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <Routes>
           <Route index element={<HomePage/>} />
           <Route path="*" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- e-comm-frontend/src/Pages/signup.js
+++ e-comm-frontend/src/Pages/signup.js
@@ -1,10 +1,49 @@
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useState } from 'react';
 
 function SignUpPage() {
 
+    // create state to store the email and password input from the client //
+
+    const [emailInput, setEmailInput] = useState('');
+    const [passwordInput, setPasswordInput] = useState('');
+
+    // create functions to hanlde the changing inputs from the client //
+
+    const handleEmailChange = (e) => {
+        setEmailInput(e.target.value)
+    }
+    const handlePasswordChange = (e) => {
+        setPasswordInput(e.target.value)
+    }
+
+    // create function to hanlde the submission of the form //
+
+    const handleSubmit = () => {
+
+    }
+
     return (
     
-        <div>Sign up Page</div>
+        <Container>
+            <Row>
+                <Col md={6} className="login-form-container">
+                    <form action="" onSubmit={handleSubmit}>
+                        <h1>
+                        Sign up
+                        </h1>
+                        <label>Email Address</label>
+                        <input type="email" placeholder="Enter Email" value={emailInput} required onChange={handleEmailChange}></input>
+                        <label>Password</label>
+                        <input type="password" placeholder="Enter Password" value={passwordInput} required onChange={handlePasswordChange}></input>
+                        <button type="submit">Login</button>
+                    </form>
+                </Col>
+                <Col md={6}></Col>
+        </Row>
+</Container>
 )
 
 }
