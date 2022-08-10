e553be4399c3ebbbc422923caf1cf96f329742c4
Daniel <daniel-mcintyre@hotmail.com>
2022-08-10 09:31:56 -0400

    additional backend setup

--- e-comm-backend/connection.js
+++ e-comm-backend/connection.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-const connectionStr = "";
+const connectionStr = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PW}@e-comm-database.i2n6ux7.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.connect(connectionStr, {useNewUserParser: true});
+mongoose.connect(connectionStr, { useNewUrlParser: true }).then(() => console.log('connected to mongoDB')).catch(err => console.log(err)); 
+
+mongoose.connection.on('error', err => {
+    console.log(err);
+})
+    
