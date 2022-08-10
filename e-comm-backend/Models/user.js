a98a7164f646715ac0b026012dbb77b4c21e5149
Daniel <daniel-mcintyre@hotmail.com>
2022-08-10 12:00:58 -0400

    updates to users model

--- e-comm-backend/Models/user.js
+++ e-comm-backend/Models/user.js
@@ -41,6 +41,36 @@ const UserSchema = Schema({
 
 })
 
+UserSchema.statics.findByCredentials = async function (email, password) {
+    const user = await User.findOne({ email });
+    if (!user) throw new Error('invalid credentials');
+    const isSamePassword = bcrypt.compareSync(password, user.password);
+    if (isSamePassword) return user;
+    throw new Error('invalid credentials');
+}
+
+UserSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toOject();
+    delete userObject.password;
+    return userObject;
+}
+
+UserSchema.pre('save', function (next) {
+    const user = this;
+    if (!user.isModified('password')) return next(); 
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) return (err);
+
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return (err);
+
+            user.password = hash;
+            next();
+    })
+    })
+})
+
 const user = mongoose.model('user', UserSchema);
 
 module.exports = user;
